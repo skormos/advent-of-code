43b2092914a70a00e77cddbd257e01fc4ace3304
Spencer Kormos <spencer@kormos.net>
2020-12-12 00:12:35 -0500

    Day 6 Part 1

--- 2020/06-custom-customs.js
+++ 2020/06-custom-customs.js
@@ -3,5 +3,14 @@ const fs = require('fs');
 fs.readFile('./06-custom-customs.txt', 'utf8', (err, data) => {
     const groups = data.split('\n\n');
 
+    const groupSets = groups.map(value => {
+        return new Set(Array.from(value.replace(/\n/g, '').split('')));
+    });
 
+    const sum = groupSets.reduce((acc, set) => {
+        return acc + set.size;
+    }, 0);
+
+    console.log("unique", sum);
 });
+
