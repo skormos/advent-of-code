82386d8a25bf8b60e72d76c28ecf12e18ec707ce
Spencer Kormos <spencer@kormos.net>
2020-12-13 21:07:11 -0500

    Day 6 part 2

--- 2020/06-custom-customs.js
+++ 2020/06-custom-customs.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+// unique questions answered for the group
 fs.readFile('./06-custom-customs.txt', 'utf8', (err, data) => {
     const groups = data.split('\n\n');
 
@@ -11,6 +12,40 @@ fs.readFile('./06-custom-customs.txt', 'utf8', (err, data) => {
         return acc + set.size;
     }, 0);
 
+    console.log("group", sum);
+});
+
+// unique questions answered by everyone in the group
+fs.readFile('./06-custom-customs.txt', 'utf8', (err, data) => {
+    const groups = data.split('\n\n');
+
+    const groupSets = groups.map(value => {
+        let members = value.split('\n');
+        let result = new Set(Array.from(members[0].split('')));
+
+        for (let i = 1; i < members.length; i++) {
+            result = intersect(result, new Set(Array.from(members[i].split(''))));
+        }
+
+        return result;
+    });
+
+    const sum = groupSets.reduce((acc, set) => {
+        return acc + set.size;
+    }, 0);
+
     console.log("unique", sum);
 });
 
+// set intersection
+function intersect(first, second) {
+    let result = new Set();
+
+    first.forEach(val => {
+        if (second.has(val)) {
+            result.add(val);
+        }
+    });
+
+    return result;
+}
